31f9f33ba596d80373edc4c5cb0b95cad07536dc
Dimitar Anguelov <d.angelov.9090@gmail.com>
2024-07-22 19:00:37 +0300

    Initial Commit

---
+++ GuitAR/GuitARApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuitARApp.swift
+//  GuitAR
+//
+//  Created by Dimitar Angelov on 22.07.24.
+//
+
+import SwiftUI
+
+@main
+struct GuitARApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
